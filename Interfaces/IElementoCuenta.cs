fe3b1af1a07e10cfd20ca629549ac2d5b6cc8c03
ᓚᘏᗢ !? <datamayo.211@gmail.com>
2023-12-18 17:57:08 -0600

    Implementación de la interfaz "IElementoCuenta"
    Según los lineamientos dados en las instrucciones, se creó la interfaz cuenta de modo en que esta reciba un platillo, cuánto de ese platillo se pidió, y lo que se va a pagar en total de la cuenta

--- Interfaces/IElementoCuenta.cs
+++ Interfaces/IElementoCuenta.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PracticaRestaurante.Interfaces;
 
 namespace PracticaRestaurante.Interfaces
 {
     public interface IElementoCuenta
     {
-        
+        public Platillo platillo { get; set; }
+        public int Cantidad { get; set; }
+
+        public int CostoTotal { get; }
     }
 
 }
