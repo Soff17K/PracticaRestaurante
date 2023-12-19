3e0879303099a3d6b92f8f017ea394d3ade4e157
ᓚᘏᗢ !? <datamayo.211@gmail.com>
2023-12-18 18:03:01 -0600

    Implementación de la interfaz "ICuenta"
    Según lo pedido por las instrucciones, la interfaz de "ICuenta" solo es una colección de elementos dados por la clase "ElementoCuenta". Además de que se calcula aquí el total de la cuenta en si.
    Se agregaron también los comportamientos de "Agregar elemento" y de "Mostrar cuenta"

--- Interfaces/ICuenta.cs
+++ Interfaces/ICuenta.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PracticaRestaurante.Interfaces;
 
 namespace PracticaRestaurante.Interfaces
 {
     public interface ICuenta
     {
     
+        private List<ElementoCuenta> _elementosCuenta;
 
+        public int Total { get; }
+
+        public void AgregarElemento(Platillo platillo, int Cantidad){
+
+        }
+
+        public void MostrarCuenta(){
+
+        }
 
     }
 
